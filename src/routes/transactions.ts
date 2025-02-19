c1a1ac6a9af96f90e662b2628d8c69b53c86719d
Rian Guedes <rianrdx0@gmail.com>
2025-02-18 23:09:28 -0300

    chore: Production config v0.0.1

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,97 +1,97 @@
-import { FastifyInstance } from "fastify";
-import { z } from "zod";
-import { randomUUID } from "node:crypto";
-import { knex } from "../database";
-import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get(
-    "/",
+    '/',
     {
       preHandler: [checkSessionIdExists],
     },
     async (request) => {
-      const { sessionId } = request.cookies;
+      const { sessionId } = request.cookies
 
-      const transactions = await knex("transactions")
-        .where("session_id", sessionId)
-        .select();
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
 
-      return { transactions };
+      return { transactions }
     },
-  );
+  )
 
   app.get(
-    "/:id",
+    '/:id',
     {
       preHandler: [checkSessionIdExists],
     },
     async (request) => {
       const getTransactionParamsSchema = z.object({
         id: z.string().uuid(),
-      });
+      })
 
-      const { id } = getTransactionParamsSchema.parse(request.params);
+      const { id } = getTransactionParamsSchema.parse(request.params)
 
-      const { sessionId } = request.cookies;
+      const { sessionId } = request.cookies
 
-      const transaction = await knex("transactions")
+      const transaction = await knex('transactions')
         .where({
           session_id: sessionId,
           id,
         })
-        .first();
+        .first()
 
-      return { transaction };
+      return { transaction }
     },
-  );
+  )
 
   app.get(
-    "/sumary",
+    '/sumary',
     {
       preHandler: [checkSessionIdExists],
     },
     async (request) => {
-      const { sessionId } = request.cookies;
+      const { sessionId } = request.cookies
 
-      const sumary = await knex("transactions")
-        .where("session_id", sessionId)
-        .sum("amount", { as: "amount" })
-        .first();
+      const sumary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
 
-      return { sumary };
+      return { sumary }
     },
-  );
+  )
 
-  app.post("/", async (request, reply) => {
+  app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
-      type: z.enum(["credit", "debit"]),
-    });
+      type: z.enum(['credit', 'debit']),
+    })
 
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
-    );
+    )
 
-    let sessionId = request.cookies.sessionId;
+    let sessionId = request.cookies.sessionId
 
     if (!sessionId) {
-      sessionId = randomUUID();
+      sessionId = randomUUID()
 
-      reply.cookie("sessionId", sessionId, {
-        path: "/",
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
         maxAge: 60 * 60 * 24 * 7, // 7 days
-      });
+      })
     }
 
-    await knex("transactions").insert({
+    await knex('transactions').insert({
       id: randomUUID(),
       title,
-      amount: type === "credit" ? amount : amount * -1,
+      amount: type === 'credit' ? amount : amount * -1,
       session_id: sessionId,
-    });
+    })
 
-    return reply.status(201).send();
-  });
+    return reply.status(201).send()
+  })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
     port: env.PORT,
   })
   .then(() => {
-    console.log("HTTP Server Running!");
-  });
+    console.log('HTTP Server Running!')
+    console.log('🚀 Iniciando o servidor na porta:', env.PORT)
+  })
